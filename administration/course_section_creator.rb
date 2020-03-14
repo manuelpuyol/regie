b4918fe74d8a6f91708fba2c53f5bcf15518551e
Manuel Puyol <manuelpuyol@gmail.com>
2020-03-14 13:40:47 -0500

    fix creator to receive professor email

--- administration/course_section_creator.rb
+++ administration/course_section_creator.rb
@@ -1,19 +1,46 @@
 # frozen_string_literal: true
 
 require './registration/course_section'
+require './authentication/user'
+require './db/errors/record_not_found'
 
 module Administration
   class CourseSectionCreator
-    def initialize(course_id:, quarter_id:)
-      @course_section = Registration::CourseSection.new(
-        course_id: course_id,
-        quarter_id: quarter_id
-      )
+    def initialize(course_id:, quarter_id:, professor_email:)
+      @course_id = course_id
+      @quarter_id = quarter_id
+      @professor_email = professor_email
     end
 
     def call
-      @course_section.save!
-      @course_section
+      if valid?
+        create
+      else
+        raise DB::Errors::RecordNotFound, "Couldn't find a professor with email #{@professor_email}"
+      end
+    end
+
+    private
+
+    def valid?
+      !professor.nil?
+    end
+
+    def create
+      course_section.save!
+      course_section
+    end
+
+    def professor
+      @professor ||= Authentication::User.where(email: @professor_email, type: Authentication::User::TYPES[:staff]).first
+    end
+
+    def course_section
+      @course_section ||= Registration::CourseSection.new(
+        course_id: @course_id,
+        quarter_id: @quarter_id,
+        professor_id: @professor.id
+      )
     end
   end
 end
--- spec/administration/course_section_creator_spec.rb
+++ spec/administration/course_section_creator_spec.rb
@@ -6,16 +6,41 @@ RSpec.describe Administration::CourseSectionCreator do
   describe '#call' do
     let(:course) { create(:course) }
     let(:quarter) { create(:quarter) }
-    let(:creator) { described_class.new(course_id: course.id, quarter_id: quarter.id) }
+    let(:creator) { described_class.new(course_id: course.id, quarter_id: quarter.id, professor_email: email) }
 
     subject { creator.call }
 
-    it 'creates a course_section' do
-      course_section = subject
+    context 'when email provided does not exist' do
+      let(:email) { 'nonexistent@uchicago.edu' }
 
-      expect(course_section.id).to_not be_nil
-      expect(course_section.course_id).to eq(course.id)
-      expect(course_section.quarter_id).to eq(quarter.id)
+      it 'raises a DB::Errors::RecordNotFound error' do
+        expect { subject }.to raise_error(DB::Errors::RecordNotFound)
+      end
+    end
+
+    context 'when email provided exists' do
+      let(:email) { user.email }
+
+      context 'when email is not from a professor' do
+        let(:user) { create(:user, :student) }
+
+        it 'raises a DB::Errors::RecordNotFound error' do
+          expect { subject }.to raise_error(DB::Errors::RecordNotFound)
+        end
+      end
+
+      context 'when email is from a professor' do
+        let(:user) { create(:user, :staff) }
+
+        it 'creates a course_section' do
+          course_section = subject
+
+          expect(course_section.id).to_not be_nil
+          expect(course_section.course_id).to eq(course.id)
+          expect(course_section.quarter_id).to eq(quarter.id)
+          expect(course_section.professor_id).to eq(user.id)
+        end
+      end
     end
   end
 end
